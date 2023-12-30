40560eff38f4c5192619d8508e8d8c70feaf235a
donghyeon choi <downpool.choi@gmail.com>
2023-12-30 17:30:41 +0900

    .

---
+++ practices.swiftpm/main.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  practices
+//
+//  Created by donghyeon choi on 12/18/23.
+//
+
+import Foundation
+
+
+let array: [Int] = [1, 2, 3, 4, 2, 3, 1, 5, 4, 12, 10, 8]
+
+for ints in array {
+    print("\(ints)")
+}
