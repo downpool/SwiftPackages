0f02c2175823a3c2abd3cb32a087c81c3524c859
donghyeon choi <downpool.choi@gmail.com>
2024-01-18 14:30:16 +0900

    init

---
+++ Pencils/Pencils/Model.swift
@@ -0,0 +1,11 @@
+//
+//  Model.swift
+//  Pencils
+//
+//  Created by donghyeon choi on 1/18/24.
+//
+
+import Foundation
+import PencilKit
+
+
---
+++ Pencils/Pencils/PencilsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PencilsApp.swift
+//  Pencils
+//
+//  Created by donghyeon choi on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct PencilsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
