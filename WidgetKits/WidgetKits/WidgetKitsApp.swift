79370baeab22bc8f6d57dde270a939206d7b5226
donghyeon choi <downpool.choi@gmail.com>
2024-03-01 16:56:13 +0900

    widgetkit init

---
+++ WidgetKits/WidgetKits/WidgetKitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitsApp.swift
+//  WidgetKits
+//
+//  Created by donghyeon choi on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
