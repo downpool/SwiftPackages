8695e5cefd56a19f21b71825ef79069a19749484
donghyeon choi <downpool.choi@gmail.com>
2024-03-04 17:17:13 +0900

    init

---
+++ WidgetKits/MainWidgetKits/MainWidgetKitsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  MainWidgetKitsBundle.swift
+//  MainWidgetKits
+//
+//  Created by donghyeon choi on 3/3/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct MainWidgetKitsBundle: WidgetBundle {
+    var body: some Widget {
+        MainWidgetKits()
+    }
+}
